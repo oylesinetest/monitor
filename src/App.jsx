4d76bfd1ce48e5fbe30bf64a11182e32897992a2
oylesinetest <ruzgarayan52@gmail.com>
2023-09-03 19:24:58 +0100

    v3

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { CircularProgress } from '@mui/material';
+import ProgressBar from "@ramonak/react-progress-bar";
 
 function tableFloatFormat (n) {
   return (n<0?"":"+") + n.toFixed(2);
@@ -88,21 +89,85 @@ function identityChange() {
   return (value) => 0;
 }
 
-function plus1Change() {
-  return (value) => 1;
+function directChange(change) {
+  return (value) => change;
+}
+
+function directChangeLimit(change, limit) {
+  return (value) => value > limit ? 0 : change;
 }
 
 function percentChange(percent) {
   return (value) => value * percent;
 }
 
+function percentChangeLimit(percent, limit) {
+  return (value) => value > limit ? 0 : value * percent;
+}
+
+function inversePercentChange(percent) {
+  return (value) => (100 - value) * percent;
+}
+
 function changeForValueAndAction(valueType, actionType) {
 
   switch (actionType) {
     case 'Faiz':
-      return plus1Change();
+      return inversePercentChange(0.02);
     case 'Kawga':
       return percentChange(-0.3);
+    case 'Asksal Olay':
+      return inversePercentChange(0.01);
+    case 'Trip':
+      return inversePercentChange(-0.05);
+    case 'Karsilikli Trip':
+      return inversePercentChange(-0.1);
+    case 'Selfie':
+      switch (valueType) {
+        case 'Buluşma':
+          return directChangeLimit(5, 50);
+        case 'Öpüşme':
+          return directChangeLimit(3, 30);
+        case 'Evlenme':
+          return directChangeLimit(1, 20);
+        default:
+          identityChange();
+      }
+    case 'Ayak Fotosu':
+      switch (valueType) {
+        case 'Buluşma':
+          return percentChangeLimit(0.3, 60);
+        case 'Öpüşme':
+          return percentChangeLimit(0.2, 70);
+        case 'Evlenme':
+          return percentChangeLimit(0.05, 50);
+        default:
+          identityChange();
+      }
+    
+      case 'Birlikte Oyun':
+        switch (valueType) {
+          case 'Buluşma':
+            return directChangeLimit(5, 50);
+          case 'Öpüşme':
+            return directChangeLimit(3, 30);
+          case 'Evlenme':
+            return directChangeLimit(2, 40);
+          default:
+            identityChange();
+        }
+            
+      case 'Birlikte Dizi':
+        switch (valueType) {
+          case 'Buluşma':
+            return directChangeLimit(1, 80);
+          case 'Öpüşme':
+            return directChangeLimit(1, 80);
+          case 'Evlenme':
+            return directChangeLimit(2, 80);
+          default:
+            identityChange();
+        }
     default:
       return identityChange();
   }
@@ -116,9 +181,9 @@ function deltaValueWithAction(value, valueType, actionType) {
 function deltaValuesWithAction(values, action) {
   const actionType = action.type;
   return {
-    bulusma: parseFloat(deltaValueWithAction(values.bulusma, 'bulusma', actionType)),
-    opusme: parseFloat(deltaValueWithAction(values.opusme, 'opusme', actionType)),
-    evlenme: parseFloat(deltaValueWithAction(values.evlenme, 'evlenme', actionType)),
+    bulusma: parseFloat(deltaValueWithAction(values.bulusma, 'Buluşma', actionType)),
+    opusme: parseFloat(deltaValueWithAction(values.opusme, 'Öpüşme', actionType)),
+    evlenme: parseFloat(deltaValueWithAction(values.evlenme, 'Evlenme', actionType)),
   }
 }
 
@@ -144,9 +209,9 @@ function App() {
   const startingDay = new Date('2023-09-01');
   const today = Date.now();
   const startingValues = {
-    bulusma: 20,
-    opusme: 10,
-    evlenme: 5,
+    bulusma: 30,
+    opusme: 20,
+    evlenme: 10,
   };
 
   const computeData = (data) => {
@@ -200,7 +265,7 @@ function App() {
         processedValues[i + 1].values = currVal;
       }
     }
-    
+
     processedValues = processedValues.map(obj => ({
       ...obj,
       bulusma: obj.values.bulusma,
@@ -234,6 +299,8 @@ function App() {
   }
 
   const onSubmitButtonClick = (event) => {
+    if (selectedAction === '' || !selectedAction) return;
+
     const apiUrl = 'https://scented-military-dinner.glitch.me/add';
 
     const postData = {
@@ -256,6 +323,12 @@ function App() {
     return dayData ? dayData.actions : [];
   }
 
+  const chosenDayValues = () => {
+    const dayData = dateFormattedData.find((data) => data.date === chosenDay);
+
+    return dayData ? dayData.values : [];
+  }
+
   return (
     <div style={{ height: "100vh", width: "100vh" }}>
       {isLoading ? (
@@ -273,20 +346,29 @@ function App() {
                 }} />
               <Tooltip />
               <Legend />
-              <Line type="monotone" dataKey="bulusma" stroke="#8884d8" />
-              <Line type="monotone" dataKey="opusme" stroke="#8884d8" />
-              <Line type="monotone" dataKey="evlenme" stroke="#8884d8" />
+              <Line type="monotone" dataKey="bulusma" stroke="#8884d8" name="Buluşma"/>
+              <Line type="monotone" dataKey="opusme" stroke="#8884d8" name="Öpüşme"/>
+              <Line type="monotone" dataKey="evlenme" stroke="#8884d8" name="Evlenme"/>
             </LineChart>
           </ResponsiveContainer>
           <div>
+            <ProgressBar completed={chosenDayValues().bulusma} maxCompleted={100} customLabel='Buluşma' height='40px' transitionDuration= '3s' animateOnRender={true}/>
+            <br></br>
+            <ProgressBar completed={chosenDayValues().opusme} maxCompleted={100} customLabel='Öpüşme' height='40px' transitionDuration= '3s' animateOnRender={true}/>
+            <br></br>
+            <ProgressBar completed={chosenDayValues().evlenme} maxCompleted={100} customLabel='Evlenme' height='40px' transitionDuration= '3s' animateOnRender={true}/>
+          </div>
+            <br></br>
+          <div>
+            
             <TableContainer component={Paper}>
               <Table sx={{ minWidth: 650 }} aria-label="simple table">
                 <TableHead>
                   <TableRow>
                     <TableCell>Olay</TableCell>
                     <TableCell align="right">Zaman</TableCell>
-                    <TableCell align="right">Bulusma</TableCell>
-                    <TableCell align="right">Opusme</TableCell>
+                    <TableCell align="right">Buluşma</TableCell>
+                    <TableCell align="right">Öpüşme</TableCell>
                     <TableCell align="right">Evlenme</TableCell>
                   </TableRow>
                 </TableHead>
@@ -309,7 +391,9 @@ function App() {
               </Table>
             </TableContainer>
           </div>
+            <br></br>
           <div>
+            <></>
             <Box sx={{ minWidth: 120 }}>
               <FormControl fullWidth>
                 <InputLabel id="demo-simple-select-label">Olay</InputLabel>
@@ -322,13 +406,17 @@ function App() {
                 >
                   <MenuItem value={'Asksal Olay'}>Asksal Olay</MenuItem>
                   <MenuItem value={'Ayak Fotosu'}>Ayak Fotosu</MenuItem>
-                  <MenuItem value={'Duz Selfie'}>Duz Selfie</MenuItem>
+                  <MenuItem value={'Selfie'}>Selfie</MenuItem>
                   <MenuItem value={'Kawga'}>Kawga</MenuItem>
                   <MenuItem value={'Trip'}>Trip</MenuItem>
+                  <MenuItem value={'Karsilikli Trip'}>Karsilikli Trip</MenuItem>
+                  <MenuItem value={'Birlikte Oyun'}>Birlikte Oyun</MenuItem>
+                  <MenuItem value={'Birlikte Dizi'}>Birlikte Dizi</MenuItem>
                 </Select>
               </FormControl>
             </Box>
-            <Button variant="contained" onClick={onSubmitButtonClick}>Olayi onayla</Button>
+            <br></br>
+            <Button variant="contained" onClick={onSubmitButtonClick}>Olay ekle</Button>
           </div>
         </div>
       )}
